78a10428316b1c3422e20f69e5d49d9a76224be4
coffeelover12396 <filipa.musinov@gmail.com>
2025-09-20 21:05:48 +0100

    corrections of font size

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,18 +15,17 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <div className="d-flex">
-              <div>
-                <WeatherIcon code={props.data.icon} size={52} />
-              </div>
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} size={52} />
+            </div>
 
-              <div>
-                <WeatherTemperature celsius={props.data.temperature} />
-              </div>
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
+
         <div className="col-6">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
